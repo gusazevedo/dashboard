6aa2a5fd0e18361498487df7aa31e2207fc5ff64
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2024-06-08 15:08:28 -0300

    [FEATURE] - Helmet meta tags

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SingIn() {
-  return <h1>Sign in page</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <h1>Sign in page</h1>
+    </>
+  )
 }
