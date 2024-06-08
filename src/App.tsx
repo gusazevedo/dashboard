c4ed7da1cb3a51d35c94a9929381a80086d0ae7c
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2024-06-08 14:54:18 -0300

    [FEATURE] - Basic routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './global.css'
 
+import { RouterProvider } from 'react-router-dom'
+
+import { routes } from './routes'
+
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      Hello Vite + ShadcnUI
-    </div>
-  );
+  return <RouterProvider router={routes} />
 }
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,3 @@
+export function SingIn() {
+  return <h1>Sign in page</h1>
+}
